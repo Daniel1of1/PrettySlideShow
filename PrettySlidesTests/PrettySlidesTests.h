184ccac24d7c32957ca69fe4c661524108bbc19b
Daniel Haight <confidence.designed@gmail.com>
2013-05-20 09:42:05 +0100

    Initial Commit

---
+++ PrettySlidesTests/PrettySlidesTests.h
@@ -0,0 +1,13 @@
+//
+//  PrettySlidesTests.h
+//  PrettySlidesTests
+//
+//  Created by Daniel Haight on 20/05/2013.
+//  Copyright (c) 2013 confidence. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface PrettySlidesTests : SenTestCase
+
+@end
---
+++ PrettySlidesTests/PrettySlidesTests.m
@@ -0,0 +1,32 @@
+//
+//  PrettySlidesTests.m
+//  PrettySlidesTests
+//
+//  Created by Daniel Haight on 20/05/2013.
+//  Copyright (c) 2013 confidence. All rights reserved.
+//
+
+#import "PrettySlidesTests.h"
+
+@implementation PrettySlidesTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in PrettySlidesTests");
+}
+
+@end
